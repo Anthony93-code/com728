29063fceae25a192c5c0a20e0478bea42a014ae0
Anthony <4nwoda65@solent.ac.uk>
2021-02-27 07:40:14 +0000

    Added code to display an ascii art robot

---
+++ Basics/Output/ascii_art.py
@@ -0,0 +1,21 @@
+# Display a box
+print("##########")
+print("#        #")
+print("#        #")
+print("##########")
+
+# Display our Robot
+# Display a box
+print("##########")
+print("#  o  o  #")
+print("#   --   #")
+print("##########")
+print("    ^^    ")
+print("    ^^    ")
+print("----^^----")
+print("    ^^    ")
+print("    ^^    ")
+print("    ^^    ")
+print("    /\    ")
+print("   /  \   ")
+print("  /    \  ")
