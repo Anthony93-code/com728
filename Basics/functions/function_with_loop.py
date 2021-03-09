eff84590a0c978a833f305c779988284907678c2
Anthony <4nwoda65@solent.ac.uk>
2021-03-09 23:05:36 +0000

    Added code to demonstrate the use of a user-defined function with a loop

---
+++ Basics/functions/function_with_loop.py
@@ -0,0 +1,16 @@
+def cross_bridge(steps):
+    # Display each step
+    for step in range(steps):
+        print("Crossed step.")
+
+    # Display message
+    if steps > 5:
+        print("The bridge is collapsing!")
+    else:
+        print("We must keep going!")
+
+# calls to function
+cross_bridge(3)
+cross_bridge(6)
+cross_bridge(1)
+cross_bridge(10)
